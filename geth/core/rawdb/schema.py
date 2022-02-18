6cf940ea1ab96d631350bbb6422de50f45d46474
James Hunsaker <hunsaker@spacetime.org>
2022-02-17 19:29:12 -0500

    add schema method

--- geth/core/rawdb/schema.py
+++ geth/core/rawdb/schema.py
@@ -9,6 +9,12 @@ def encode_block_number(number: int) -> bytes:
     return struct.pack(">Q", number)
 
 
+def header_key(number: int, hash: bytes) -> bytes:
+
+    num = encode_block_number(number)
+    return b"".join([HEADER_PREFIX, num, hash])
+
+
 def header_hash_key(number: int) -> bytes:
 
     num = encode_block_number(number)
