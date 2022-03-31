c34834116d1a84d73370490f43b6d2744a640cd5
Keone Hon <keone.hon@gmail.com>
2022-03-31 12:12:53 -0500

    read_header: fix nonce decoding; add comments reflected from block.go (#1)

--- geth/core/rawdb/accessors_chain.py
+++ geth/core/rawdb/accessors_chain.py
@@ -33,21 +33,25 @@ def read_header(db, hash: bytes, number: int) -> dict:
     data = read_header_rlp(db, hash=hash, number=number)
     header_data = rlp.decode_raw(data, strict=True, preserve_cache_info=False)
     header_data = header_data[0]
+
+    # names translated from go code:
+    # https://github.com/ethereum/go-ethereum/blob/master/core/types/block.go#L70-L95
+    # https://ethereum.github.io/yellowpaper/paper.pdf
     header = {
-        "parent_hash": HexBytes(header_data[0]),
-        "uncle_hash": HexBytes(header_data[1]),
-        "coinbase": HexBytes(header_data[2]),
-        "root": HexBytes(header_data[3]),
-        "tx_hash": HexBytes(header_data[4]),
-        "receipt_hash": HexBytes(header_data[5]),
-        "bloom": HexBytes(header_data[6]),
-        "difficulty": _decode_unsigned(header_data[7]),
-        "number": _decode_unsigned(header_data[8]),
-        "gas_limit": _decode_unsigned(header_data[9]),
-        "gas_used": _decode_unsigned(header_data[10]),
-        "time": _decode_unsigned(header_data[11]),
-        "extra": header_data[12],
-        "mix_digest": HexBytes(header_data[13]),
-        "nonce": HexBytes(header_data[14]),
+        "parent_hash": HexBytes(header_data[0]),         # 256-bit hash; `json:"parentHash"       gencodec:"required"`
+        "uncle_hash": HexBytes(header_data[1]),          # 256-bit hash; `json:"sha3Uncles"       gencodec:"required"`
+        "coinbase": HexBytes(header_data[2]),            # 160-bit addr; `json:"miner"            gencodec:"required"`
+        "root": HexBytes(header_data[3]),                # 256-bit hash; `json:"stateRoot"        gencodec:"required"`
+        "tx_hash": HexBytes(header_data[4]),             # 256-bit hash; `json:"transactionsRoot" gencodec:"required"`
+        "receipt_hash": HexBytes(header_data[5]),        # 256-bit hash; `json:"receiptsRoot"     gencodec:"required"`
+        "bloom": HexBytes(header_data[6]),               # 2048-bit val; `json:"logsBloom"        gencodec:"required"`
+        "difficulty": _decode_unsigned(header_data[7]),  # bigint;       `json:"difficulty"       gencodec:"required"`
+        "number": _decode_unsigned(header_data[8]),      # bigint;       `json:"number"           gencodec:"required"`
+        "gas_limit": _decode_unsigned(header_data[9]),   # uint64;       `json:"gasLimit"         gencodec:"required"`
+        "gas_used": _decode_unsigned(header_data[10]),   # uint64;       `json:"gasUsed"          gencodec:"required"`
+        "time": _decode_unsigned(header_data[11]),       # uint64;       `json:"timestamp"        gencodec:"required"`
+        "extra": header_data[12],                        # 32 bytes;     `json:"extraData"        gencodec:"required"`
+        "mix_digest": HexBytes(header_data[13]),         # 256-bit hash; `json:"mixHash"`
+        "nonce": _decode_unsigned(header_data[14]),      # 64-bit val;   `json:"nonce"`
     }
     return header
